c0b7170d2ae663d50ff85c0c9f8ae793280542bd
shashank <shashank.srivastava5523@gmail.com>
2022-08-26 19:20:25 +0530

    5th commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import Home from "./pages/home/Home";
 import "./app.scss"
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Login from "./pages/login/Login"
+import Register from "./pages/register/Register"
+import Watch from "./pages/watch/Watch"
+
 
 const App = () => {
-  return <Home/>;
+  return (
+    <div>
+
+  <BrowserRouter>
+  <Routes>
+        <Route path='/'  element={<Home/>}></Route>
+        <Route path='/login'  element={<Login/>}></Route>
+        <Route path='/register'  element={<Register/>}></Route>
+        <Route path='/watch'  element={<Watch/>}></Route>
+    </Routes>
+  </BrowserRouter>
+
+  </div>
+  );
 };  
 
 export default App
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
   };
   return (
     <div className={isScrolled ? "navbar scrolled" : "navbar"}>
-        sdjfklsd
+   
       <div className="container">
         <div className="left">
           <img
